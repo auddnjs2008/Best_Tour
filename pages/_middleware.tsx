07fdeb92eb8311b7f3457fc7952ce40898359116
M.W <auddnjs2008@naver.com>
2022-05-20 15:05:29 +0900

    index 페이지 삭제

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -6,6 +6,7 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
         if (!req.url.includes("/signup") && !req.url.includes("/enter") && !req.cookies.besttoursession) {
             return NextResponse.redirect(new URL("/enter", req.url));
         }
+
     }
     return;
 }
